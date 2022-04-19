56853d33a8640733b62fa3fd561e8e9eb5b3e8e1
Carlos Santana <santanacarlos.ar@gmail.com>
2022-04-18 23:41:46 -0500

    Agregar archivos de proyecto.

---
+++ TransposicionPorFilas/Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace TransposicionPorFilas {
+    partial class Form1 {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.grpAlgoritmo = new System.Windows.Forms.GroupBox();
+            this.radDobleClave = new System.Windows.Forms.RadioButton();
+            this.radDoble = new System.Windows.Forms.RadioButton();
+            this.radSimpleClave = new System.Windows.Forms.RadioButton();
+            this.radSimple = new System.Windows.Forms.RadioButton();
+            this.grpOpcion = new System.Windows.Forms.GroupBox();
+            this.radDescifrar = new System.Windows.Forms.RadioButton();
+            this.radCifrar = new System.Windows.Forms.RadioButton();
+            this.lblMensajeClaro = new System.Windows.Forms.Label();
+            this.txtMensajeClaro = new System.Windows.Forms.TextBox();
+            this.lblNRenglones = new System.Windows.Forms.Label();
+            this.numRenglones = new System.Windows.Forms.NumericUpDown();
+            this.lblClave = new System.Windows.Forms.Label();
+            this.txtClave = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.btnAccionar = new System.Windows.Forms.Button();
+            this.grpAlgoritmo.SuspendLayout();
+            this.grpOpcion.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numRenglones)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblTitulo.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblTitulo.Location = new System.Drawing.Point(0, 0);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(736, 46);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Transposición Por Filas";
+            this.lblTitulo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // grpAlgoritmo
+            // 
+            this.grpAlgoritmo.Controls.Add(this.radDobleClave);
+            this.grpAlgoritmo.Controls.Add(this.radDoble);
+            this.grpAlgoritmo.Controls.Add(this.radSimpleClave);
+            this.grpAlgoritmo.Controls.Add(this.radSimple);
+            this.grpAlgoritmo.Location = new System.Drawing.Point(12, 49);
+            this.grpAlgoritmo.Name = "grpAlgoritmo";
+            this.grpAlgoritmo.Size = new System.Drawing.Size(149, 124);
+            this.grpAlgoritmo.TabIndex = 1;
+            this.grpAlgoritmo.TabStop = false;
+            this.grpAlgoritmo.Text = "Algoritmo";
+            // 
+            // radDobleClave
+            // 
+            this.radDobleClave.AutoSize = true;
+            this.radDobleClave.Location = new System.Drawing.Point(6, 97);
+            this.radDobleClave.Name = "radDobleClave";
+            this.radDobleClave.Size = new System.Drawing.Size(99, 19);
+            this.radDobleClave.TabIndex = 3;
+            this.radDobleClave.TabStop = true;
+            this.radDobleClave.Text = "Doble + Clave";
+            this.radDobleClave.UseVisualStyleBackColor = true;
+            this.radDobleClave.CheckedChanged += new System.EventHandler(this.cambiarAlgoritmo);
+            // 
+            // radDoble
+            // 
+            this.radDoble.AutoSize = true;
+            this.radDoble.Location = new System.Drawing.Point(6, 72);
+            this.radDoble.Name = "radDoble";
+            this.radDoble.Size = new System.Drawing.Size(56, 19);
+            this.radDoble.TabIndex = 2;
+            this.radDoble.TabStop = true;
+            this.radDoble.Text = "Doble";
+            this.radDoble.UseVisualStyleBackColor = true;
+            this.radDoble.CheckedChanged += new System.EventHandler(this.cambiarAlgoritmo);
+            // 
+            // radSimpleClave
+            // 
+            this.radSimpleClave.AutoSize = true;
+            this.radSimpleClave.Location = new System.Drawing.Point(6, 47);
+            this.radSimpleClave.Name = "radSimpleClave";
+            this.radSimpleClave.Size = new System.Drawing.Size(104, 19);
+            this.radSimpleClave.TabIndex = 1;
+            this.radSimpleClave.TabStop = true;
+            this.radSimpleClave.Text = "Simple + Clave";
+            this.radSimpleClave.UseVisualStyleBackColor = true;
+            this.radSimpleClave.CheckedChanged += new System.EventHandler(this.cambiarAlgoritmo);
+            // 
+            // radSimple
+            // 
+            this.radSimple.AutoSize = true;
+            this.radSimple.Checked = true;
+            this.radSimple.Location = new System.Drawing.Point(6, 22);
+            this.radSimple.Name = "radSimple";
+            this.radSimple.Size = new System.Drawing.Size(61, 19);
+            this.radSimple.TabIndex = 0;
+            this.radSimple.TabStop = true;
+            this.radSimple.Text = "Simple";
+            this.radSimple.UseVisualStyleBackColor = true;
+            this.radSimple.CheckedChanged += new System.EventHandler(this.cambiarAlgoritmo);
+            // 
+            // grpOpcion
+            // 
+            this.grpOpcion.Controls.Add(this.radDescifrar);
+            this.grpOpcion.Controls.Add(this.radCifrar);
+            this.grpOpcion.Location = new System.Drawing.Point(167, 49);
+            this.grpOpcion.Name = "grpOpcion";
+            this.grpOpcion.Size = new System.Drawing.Size(90, 124);
+            this.grpOpcion.TabIndex = 2;
+            this.grpOpcion.TabStop = false;
+            this.grpOpcion.Text = "Opción";
+            // 
+            // radDescifrar
+            // 
+            this.radDescifrar.AutoSize = true;
+            this.radDescifrar.Location = new System.Drawing.Point(6, 72);
+            this.radDescifrar.Name = "radDescifrar";
+            this.radDescifrar.Size = new System.Drawing.Size(71, 19);
+            this.radDescifrar.TabIndex = 1;
+            this.radDescifrar.Text = "Descifrar";
+            this.radDescifrar.UseVisualStyleBackColor = true;
+            this.radDescifrar.CheckedChanged += new System.EventHandler(this.cambiarAccion);
+            // 
+            // radCifrar
+            // 
+            this.radCifrar.AutoSize = true;
+            this.radCifrar.Checked = true;
+            this.radCifrar.Location = new System.Drawing.Point(6, 47);
+            this.radCifrar.Name = "radCifrar";
+            this.radCifrar.Size = new System.Drawing.Size(54, 19);
+            this.radCifrar.TabIndex = 0;
+            this.radCifrar.TabStop = true;
+            this.radCifrar.Text = "Cifrar";
+            this.radCifrar.UseVisualStyleBackColor = true;
+            this.radCifrar.CheckedChanged += new System.EventHandler(this.cambiarAccion);
+            // 
+            // lblMensajeClaro
+            // 
+            this.lblMensajeClaro.AutoSize = true;
+            this.lblMensajeClaro.Location = new System.Drawing.Point(271, 53);
+            this.lblMensajeClaro.Name = "lblMensajeClaro";
+            this.lblMensajeClaro.Size = new System.Drawing.Size(54, 15);
+            this.lblMensajeClaro.TabIndex = 3;
+            this.lblMensajeClaro.Text = "Mensaje:";
+            // 
+            // txtMensajeClaro
+            // 
+            this.txtMensajeClaro.Location = new System.Drawing.Point(339, 50);
+            this.txtMensajeClaro.Name = "txtMensajeClaro";
+            this.txtMensajeClaro.Size = new System.Drawing.Size(385, 23);
+            this.txtMensajeClaro.TabIndex = 4;
+            // 
+            // lblNRenglones
+            // 
+            this.lblNRenglones.AutoSize = true;
+            this.lblNRenglones.Location = new System.Drawing.Point(271, 82);
+            this.lblNRenglones.Name = "lblNRenglones";
+            this.lblNRenglones.Size = new System.Drawing.Size(111, 15);
+            this.lblNRenglones.TabIndex = 5;
+            this.lblNRenglones.Text = "Num. de renglones:";
+            // 
+            // numRenglones
+            // 
+            this.numRenglones.Location = new System.Drawing.Point(388, 80);
+            this.numRenglones.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numRenglones.Name = "numRenglones";
+            this.numRenglones.Size = new System.Drawing.Size(45, 23);
+            this.numRenglones.TabIndex = 6;
+            this.numRenglones.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // lblClave
+            // 
+            this.lblClave.AutoSize = true;
+            this.lblClave.Location = new System.Drawing.Point(451, 82);
+            this.lblClave.Name = "lblClave";
+            this.lblClave.Size = new System.Drawing.Size(39, 15);
+            this.lblClave.TabIndex = 7;
+            this.lblClave.Text = "Clave:";
+            // 
+            // txtClave
+            // 
+            this.txtClave.Enabled = false;
+            this.txtClave.Location = new System.Drawing.Point(496, 79);
+            this.txtClave.Name = "txtClave";
+            this.txtClave.Size = new System.Drawing.Size(133, 23);
+            this.txtClave.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(271, 142);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 15);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Resultado:";
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(339, 142);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.ReadOnly = true;
+            this.txtResultado.Size = new System.Drawing.Size(385, 23);
+            this.txtResultado.TabIndex = 10;
+            // 
+            // btnAccionar
+            // 
+            this.btnAccionar.BackColor = System.Drawing.Color.Chartreuse;
+            this.btnAccionar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAccionar.Location = new System.Drawing.Point(635, 78);
+            this.btnAccionar.Name = "btnAccionar";
+            this.btnAccionar.Size = new System.Drawing.Size(89, 25);
+            this.btnAccionar.TabIndex = 11;
+            this.btnAccionar.Text = "Cifrar";
+            this.btnAccionar.UseVisualStyleBackColor = false;
+            this.btnAccionar.Click += new System.EventHandler(this.btnAccionar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(736, 178);
+            this.Controls.Add(this.btnAccionar);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtClave);
+            this.Controls.Add(this.lblClave);
+            this.Controls.Add(this.numRenglones);
+            this.Controls.Add(this.lblNRenglones);
+            this.Controls.Add(this.txtMensajeClaro);
+            this.Controls.Add(this.lblMensajeClaro);
+            this.Controls.Add(this.grpOpcion);
+            this.Controls.Add(this.grpAlgoritmo);
+            this.Controls.Add(this.lblTitulo);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Transposición Por Filas";
+            this.grpAlgoritmo.ResumeLayout(false);
+            this.grpAlgoritmo.PerformLayout();
+            this.grpOpcion.ResumeLayout(false);
+            this.grpOpcion.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numRenglones)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.GroupBox grpAlgoritmo;
+        private System.Windows.Forms.RadioButton radDobleClave;
+        private System.Windows.Forms.RadioButton radDoble;
+        private System.Windows.Forms.RadioButton radSimpleClave;
+        private System.Windows.Forms.RadioButton radSimple;
+        private System.Windows.Forms.GroupBox grpOpcion;
+        private System.Windows.Forms.RadioButton radDescifrar;
+        private System.Windows.Forms.RadioButton radCifrar;
+        private System.Windows.Forms.Label lblMensajeClaro;
+        private System.Windows.Forms.TextBox txtMensajeClaro;
+        private System.Windows.Forms.Label lblNRenglones;
+        private System.Windows.Forms.NumericUpDown numRenglones;
+        private System.Windows.Forms.Label lblClave;
+        private System.Windows.Forms.TextBox txtClave;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Button btnAccionar;
+    }
+}
---
+++ TransposicionPorFilas/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TransposicionPorFilas {
+    public partial class Form1: Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void cambiarAlgoritmo(object sender, EventArgs e) {
+            if(((RadioButton) sender).Text.Contains("Clave")) {
+                txtClave.Clear();
+                txtClave.Enabled = true;
+                numRenglones.Enabled = false;
+            }
+            else {
+                numRenglones.Value = 1;
+                txtClave.Enabled = false;
+                numRenglones.Enabled = true;
+            }
+        }
+
+        private void cambiarAccion(object sender, EventArgs e) {
+            if(radCifrar.Checked)
+                btnAccionar.Text = "Cifrar";
+            else
+                btnAccionar.Text = "Descifrar";
+        }
+
+        private void btnAccionar_Click(object sender, EventArgs e) {
+            txtMensajeClaro.Text = txtMensajeClaro.Text.Trim();
+            int CantidadRenglones;
+            if(radSimpleClave.Checked || radDobleClave.Checked) {
+                //Validar que la clave cumpla con los requisitos.
+                if(txtClave.Text == null || txtClave.Text.Trim().Length == 0) {
+                    MessageBox.Show("Ingrese una clave válida. No puede dejar la clave vacia", "Clave no válida");
+                    return;
+                }
+                for(int i = 0; i < txtClave.Text.Length; i++) {
+                    if(txtClave.Text.IndexOf(txtClave.Text[i]) != txtClave.Text.LastIndexOf(txtClave.Text[i])) {
+                        MessageBox.Show("Ingrese una clave válida. No puede tener caracteres repetidos en la clave", "Clave no válida");
+                        return;
+                    }
+                }
+                CantidadRenglones = txtClave.Text.Length;
+            } else {
+                CantidadRenglones = (int)numRenglones.Value;
+
+            }
+            string mensaje;
+            string Resultado;
+            bool contieneClave = radSimpleClave.Checked;
+
+            if(radCifrar.Checked) {
+                if(radSimpleClave.Checked)
+                    mensaje = txtClave.Text.Trim() + txtMensajeClaro.Text;
+                else
+                    mensaje = txtMensajeClaro.Text;
+                Resultado = Cifrar(mensaje, CantidadRenglones, contieneClave);
+                if(radDoble.Checked || radDobleClave.Checked) {
+                    Resultado = txtClave.Text.Trim() + Resultado;
+                    Resultado = Cifrar(Resultado, CantidadRenglones, contieneClave || radDobleClave.Checked);
+                }
+            } else {
+                mensaje = txtMensajeClaro.Text;
+                Resultado = Descifrar(mensaje, CantidadRenglones, contieneClave || radDobleClave.Checked);
+                if(radDoble.Checked || radDobleClave.Checked) {
+                    Resultado = Descifrar(Resultado, CantidadRenglones, contieneClave);
+                }
+            }
+            txtResultado.Text = Resultado;
+        }
+
+        private string Cifrar(string mensaje, int renglones, bool contieneClave) {
+            int columnas = mensaje.Length % renglones == 0 ? (mensaje.Length / renglones) : (mensaje.Length / renglones) + 1;
+            char[][] matriz = new char[renglones][];
+            for(int i = 0; i < matriz.Length; i++) 
+                matriz[i] = new char[columnas];
+            int indice = 0;
+
+            for(int i = 0; i < columnas; i++) {
+                for(int j = 0; j < renglones; j++) {
+                    try {
+                        //Se llena la matriz de abajo hacia arriba y de izquierda a derecha
+                        matriz[j][i] = mensaje[indice];
+                    } catch { matriz[j][i] = 'X'; }
+                    indice++;
+                }
+            }
+            //Si se está usando clave ordenamos las filas de manera alfabetica.
+            if(contieneClave)
+                matriz = OrdenarMatriz(matriz, OrdenarClave(txtClave.Text.Trim()));
+            string criptograma = "";
+            //Construcción del criptograma, se mueve de izquierda a derecha y de arriba hacia abajo
+            for(int i = 0; i < renglones; i++) {
+                for(int j = contieneClave ? 1 : 0; j < columnas; j++) {
+                    criptograma += matriz[i][j];
+                }
+            }
+            return criptograma;
+        }
+
+        private string Descifrar(string criptograma, int renglones, bool contieneClave) {
+            int columnas = criptograma.Length % renglones == 0 ? (criptograma.Length / renglones) : (criptograma.Length / renglones) + 1;
+            char[][] matriz = new char[renglones][];
+            for(int i = 0; i < matriz.Length; i++) 
+                matriz[i] = new char[columnas];
+            int indice = 0;
+
+            for(int i = 0; i < renglones; i++) {
+                for(int j = 0; j < columnas; j++) {
+                    try {
+                        matriz[i][j] = criptograma[indice];
+                    } catch { matriz[i][j] = 'X'; }
+                    indice++;
+                }
+            }
+            //Si se está usando clave ordenamos las filas de manera alfabetica.
+            if(contieneClave)
+                matriz = OrdenarMatriz(matriz, txtClave.Text.Trim());
+            string mensaje = "";
+            for(int i = 0; i < columnas; i++) {
+                for(int j = 0; j < renglones; j++) {
+                    mensaje += matriz[j][i];
+                }
+            }
+            //Si se maneja una llave se recorta la llave para retornar solo el mensaje
+            if(contieneClave)
+                mensaje = mensaje.Substring(renglones);
+            return mensaje;
+        }
+        private string OrdenarClave(string clave) {
+            return String.Concat(clave.OrderBy(c => c));
+        }
+        private char[][] OrdenarMatriz(char[][] matriz, string clave) {
+            char[][] nuevaMatriz = new char[matriz.Length][];
+            int indice = 0;
+            foreach(var caracter in clave) {
+                for(int i = 0; i < matriz.Length; i++) {
+                    if(matriz[i][0] == caracter) {
+                        nuevaMatriz[indice] = matriz[i];
+                        nuevaMatriz[indice][0] = caracter;
+                        indice++;
+                    }
+                }
+            }
+            return nuevaMatriz;
+        }
+    }
+}
